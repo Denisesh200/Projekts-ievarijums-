73b7799f1b2af613cfc0fe99e6ecb15f3b9fc199
Denisesh200 <bulcenkod979@gmail.com>
2026-09-21 08:52:45 +0300

    123123dsa

--- ievarijums.py
+++ ievarijums.py
@@ -1,3 +1,3 @@
-cukura_cena=input("Ievadi cukura cenu: ")
-abolu_kg=input("Ievadi abolu kg: ")
-print(f"Par cukuru tu samaksasi{cukura_cena*abolu_kg*0.7} eiro")
+cukura_cena=float(input("Ievadi cukura cenu: "))
+abolu_kg=float(input("Ievadi abolu kg: "))
+print(f"Par cukuru tu samaksasi: {cukura_cena*abolu_kg*0.7} eiro")
